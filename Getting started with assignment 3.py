59f71a7d19f46071ee65adfb79013189736eebf4
nielsvanderpoel <n.v.d.poel@student.tue.nl>
2025-03-17 17:57:34 +0100

    Added getting started file and network file

---
+++ Getting started with assignment 3.py
@@ -0,0 +1,204 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+from shapely.geometry.linestring import LineString
+
+
+
+################################
+# The assignment graph
+################################
+
+assignmentGraph = nx.read_gml('networkAssignment.gml')
+
+
+# Show the node data
+print('Assignment Graph\n----------------\n')
+print(f'Number of nodes: {len(assignmentGraph.nodes)}')
+for k in assignmentGraph.nodes:
+    print(f"{k} respresents {assignmentGraph.nodes[k]['name']}")
+    
+# Show the edge data
+print(f'Number of edges: {len(assignmentGraph.edges)}')
+for k in assignmentGraph.edges:
+    en = assignmentGraph.edges[k]
+    print(f"{assignmentGraph.edges[k]['name']} has {assignmentGraph.edges[k]['lanes']} lanes")
+    
+
+
+
+
+# Find a route in the network
+allJunctions = list(assignmentGraph.nodes)
+fromJunction = allJunctions[1]  # Leenderheide
+toJunction = allJunctions[14]     # Ridderkerk
+rt = nx.shortest_path(assignmentGraph,fromJunction,toJunction, weight='length')
+rtLength = nx.shortest_path_length(assignmentGraph,fromJunction,toJunction, weight='length') / 1000
+
+
+# Print the route
+print(f"Assignment Graph Route from {assignmentGraph.nodes[fromJunction]['name']} to {assignmentGraph.nodes[toJunction]['name']}:")
+print(f'Total route length: {rtLength} km')
+for i in range(len(rt) - 1):
+    j1 = assignmentGraph.nodes[rt[i]]['name']
+    j2 = assignmentGraph.nodes[rt[i + 1]]['name']
+    
+    # An edge in the assignment graph is represented by a tuple of 2 nodes.
+    # Remember: this graph is UNdirected!
+    
+    edg = (rt[i], rt[i + 1])
+    length = assignmentGraph.edges[edg]['length'] / 1000  # convert meters to km
+    nrLanes = 0
+    if 'lanes' in assignmentGraph.edges[edg].keys(): 
+        nrLanes = int(assignmentGraph.edges[edg]['lanes'] )
+    print(f'{j1} -> {j2} : length = {length} km, nr of lanes = {nrLanes}')
+    
+
+
+    
+
+# Plot the assignment graph and the route
+
+posDict = {}
+junctionLabelsDict = {}
+for k in assignmentGraph.nodes:
+    posDict[k] = (assignmentGraph.nodes[k]['x'], assignmentGraph.nodes[k]['y'])
+    junctionLabelsDict[k] = assignmentGraph.nodes[k]['name']
+
+route_coords = np.array([posDict[r] for r in rt])
+
+plt.figure(figsize=(10,10))
+nx.draw(assignmentGraph, pos=posDict, labels=junctionLabelsDict, node_size=300, arrows=False)
+plt.plot(route_coords[:,0], route_coords[:,1], 'r', linewidth=5)
+plt.show()
+
+    
+
+
+
+# Some other useful NetworkX functions, applied to the assignment graph
+
+
+# Find node id of a given name
+
+ids = [n for n in assignmentGraph.nodes if assignmentGraph.nodes[n]['name'] == 'Knooppunt Batadorp']
+print(ids[0])
+
+# get all the ids plus names in a dictionary:
+print(assignmentGraph.nodes.data('name'))
+
+# same for the edge lengths
+print(assignmentGraph.edges.data('length'))
+
+
+# create a copy of a graph and add a property 'travel time' to each edge
+maxSpeed = 100  # km/h
+
+assignmentGraph2 = nx.Graph(assignmentGraph)
+for e in assignmentGraph2.edges:
+    assignmentGraph2.edges[e]['traveltime'] = assignmentGraph2.edges[e]['length'] / 1000 / maxSpeed
+
+
+rt = nx.shortest_path(assignmentGraph2,fromJunction,toJunction, weight='traveltime')
+rtTime = nx.shortest_path_length(assignmentGraph2,fromJunction,toJunction, weight='traveltime') * 60 # route travel time in minutes
+print(f'Time to travel route is: {rtTime}  minutes')
+
+
+
+
+##########################################################################################################
+#
+# Everything below this line is OPTIONAL! You do not have to use these networks,
+# but you might find this more challenging.
+# - networkNLjunctions.gml really has the exact same structure as networkAssignment.gml,
+#   but it contains ALL junctions and links in the Netherlands, not just the selected area.
+# - networkNLcomplete.gml also contains the map of the Netherlands, but has MANY more junctions
+#   and links. It does contain the actual graphical layout of all the roads, as you can see in the plot.
+#
+##########################################################################################################
+
+
+
+
+##########################################################
+# The graph with all major junctions in The Netherlands
+##########################################################
+
+junctionGraph = nx.read_gml('networkNLjunctions.gml')
+
+# Plot the junction graph and the route
+
+posDict = {}
+for k in junctionGraph.nodes:
+    posDict[k] = (junctionGraph.nodes[k]['x'], junctionGraph.nodes[k]['y'])
+
+route_coords = np.array([posDict[r] for r in rt])
+
+plt.figure(figsize=(10,10))
+nx.draw(junctionGraph, pos=posDict, node_size=100, arrows=False)
+plt.plot(route_coords[:,0], route_coords[:,1], 'r', linewidth=5)
+plt.show()
+
+    
+
+
+
+
+
+
+################################
+# The full network
+################################
+
+
+def linestring_destringizer(x):
+    '''
+    Takes string x and converts it to an object.
+    In this case, only LineStrings will be converted.
+
+    Parameters
+    ----------
+    x : str
+        The string that will be converted.
+
+    '''
+    if x[0:10] == 'LINESTRING':  # convert to linestring
+        s1 = x[12:-1].split(', ')
+        pts = []
+        for p in s1:
+            pt = p.split(' ')
+            pts.append((float(pt[0]),float(pt[1])))
+        return LineString(pts)
+
+    return str(x)
+
+
+networkGraph = nx.read_gml('networkNLcomplete.gml', destringizer=linestring_destringizer)
+
+
+posDict2 = {}
+for k in networkGraph.nodes:
+    posDict2[k] = (networkGraph.nodes[k]['x'], networkGraph.nodes[k]['y'])
+
+plt.figure(figsize=(10,10))
+nx.draw(networkGraph, pos=posDict2, node_size=0, arrows=False)
+plt.show()
+
+
+# compare the junctions on the route we studied earlier:
+# 1. assignment graph
+rt = nx.shortest_path(assignmentGraph,fromJunction,toJunction, weight='length')
+print(rt)
+
+# 2. full network graph
+rt2 = nx.shortest_path(networkGraph,fromJunction,toJunction, weight='length')
+print(rt2)
+
+
+
+
+
+
+
+
+
---
+++ networkAssignment.gml
@@ -0,0 +1,307 @@
+graph [
+  crs "epsg:4326"
+  node [
+    id 0
+    label "672759992"
+    x 5.4071021
+    y 51.4835625
+    highway "motorway_junction"
+    name "Knooppunt Batadorp"
+  ]
+  node [
+    id 1
+    label "42549870"
+    x 5.503674
+    y 51.4056199
+    highway "motorway_junction"
+    name "Knooppunt Leenderheide"
+  ]
+  node [
+    id 2
+    label "44708181"
+    x 5.0967771
+    y 51.9806966
+    highway "motorway_junction"
+    name "Knooppunt Everdingen"
+  ]
+  node [
+    id 3
+    label "1558293339"
+    x 4.8849457
+    y 51.6875869
+    highway "motorway_junction"
+    name "Knooppunt Hooipolder"
+  ]
+  node [
+    id 4
+    label "44996729"
+    x 4.3496241
+    y 52.0438636
+    highway "motorway_junction"
+    name "Knooppunt Ypenburg"
+  ]
+  node [
+    id 5
+    label "2752332143"
+    x 4.9503331
+    y 51.8463361
+    highway "motorway_junction"
+    name "Knooppunt Gorinchem"
+  ]
+  node [
+    id 6
+    label "44112758"
+    x 5.228183
+    y 51.8577737
+    highway "motorway_junction"
+    name "Knooppunt Deil"
+  ]
+  node [
+    id 7
+    label "42995944"
+    x 5.1223028
+    y 51.5498645
+    highway "motorway_junction"
+    name "Knooppunt De Baars"
+  ]
+  node [
+    id 8
+    label "43349094"
+    x 4.6924072
+    y 51.6446439
+    highway "motorway_junction"
+    name "Knooppunt Zonzeel"
+  ]
+  node [
+    id 9
+    label "43655900"
+    x 5.3216643
+    y 51.7260201
+    highway "motorway_junction"
+    name "Knooppunt Empel"
+  ]
+  node [
+    id 10
+    label "43108886"
+    x 4.7196004
+    y 51.580877
+    highway "motorway_junction"
+    name "Knooppunt Princeville"
+  ]
+  node [
+    id 11
+    label "8432860337"
+    x 4.5292215
+    y 51.9486897
+    highway "motorway_junction"
+    name "Knooppunt Terbregseplein"
+  ]
+  node [
+    id 12
+    label "43003075"
+    x 4.8335123
+    y 51.5518283
+    highway "motorway_junction"
+    name "Knooppunt Sint-Annabosch"
+  ]
+  node [
+    id 13
+    label "1410566272"
+    x 4.6618705
+    y 52.024019
+    highway "motorway_junction"
+    name "Knooppunt Gouwe"
+  ]
+  node [
+    id 14
+    label "44153932"
+    x 4.5820693
+    y 51.8668156
+    highway "motorway_junction"
+    name "Knooppunt Ridderkerk"
+  ]
+  node [
+    id 15
+    label "45098337"
+    x 5.0698981
+    y 52.0660163
+    highway "motorway_junction"
+    name "Knooppunt Oudenrijn"
+  ]
+  edge [
+    source 0
+    target 7
+    name "Knooppunt Batadorp->Knooppunt De Baars"
+    highway "motorway_link"
+    length 16923.0
+    lanes 2
+  ]
+  edge [
+    source 0
+    target 9
+    name "Knooppunt Batadorp->Knooppunt Empel"
+    highway "motorway_link"
+    length 26068.0
+    lanes 4
+  ]
+  edge [
+    source 0
+    target 1
+    name "Knooppunt Batadorp->Knooppunt Leenderheide"
+    highway "motorway_link"
+    length 12670.0
+    lanes 4
+  ]
+  edge [
+    source 2
+    target 6
+    name "Knooppunt Everdingen->Knooppunt Deil"
+    highway "motorway_link"
+    length 11002.0
+    lanes 5
+  ]
+  edge [
+    source 2
+    target 5
+    name "Knooppunt Everdingen->Knooppunt Gorinchem"
+    highway "motorway_link"
+    length 17801.0
+    lanes 2
+  ]
+  edge [
+    source 2
+    target 15
+    name "Knooppunt Everdingen->Knooppunt Oudenrijn"
+    highway "motorway_link"
+    length 5424.0
+    lanes 5
+  ]
+  edge [
+    source 3
+    target 5
+    name "Knooppunt Hooipolder->Knooppunt Gorinchem"
+    highway "motorway_link"
+    length 13084.0
+    lanes 2
+  ]
+  edge [
+    source 3
+    target 12
+    name "Knooppunt Hooipolder->Knooppunt Sint-Annabosch"
+    highway "motorway_link"
+    length 13602.0
+    lanes 2
+  ]
+  edge [
+    source 3
+    target 8
+    name "Knooppunt Hooipolder->Knooppunt Zonzeel"
+    highway "motorway_link"
+    length 10375.0
+    lanes 2
+  ]
+  edge [
+    source 3
+    target 9
+    name "Knooppunt Hooipolder->Knooppunt Empel"
+    highway "motorway_link"
+    length 24547.0
+    lanes 2
+  ]
+  edge [
+    source 4
+    target 11
+    name "Knooppunt Ypenburg->Knooppunt Terbregseplein"
+    highway "motorway_link"
+    length 12673.0
+    lanes 4
+  ]
+  edge [
+    source 4
+    target 13
+    name "Knooppunt Ypenburg->Knooppunt Gouwe"
+    highway "motorway_link"
+    length 16279.0
+    lanes 4
+  ]
+  edge [
+    source 5
+    target 6
+    name "Knooppunt Gorinchem->Knooppunt Deil"
+    highway "motorway_link"
+    length 15636.0
+    lanes 2
+  ]
+  edge [
+    source 5
+    target 14
+    name "Knooppunt Gorinchem->Knooppunt Ridderkerk"
+    highway "motorway_link"
+    length 19319.0
+    lanes 2
+  ]
+  edge [
+    source 6
+    target 9
+    name "Knooppunt Deil->Knooppunt Empel"
+    highway "motorway_link"
+    length 12514.0
+    lanes 4
+  ]
+  edge [
+    source 7
+    target 12
+    name "Knooppunt De Baars->Knooppunt Sint-Annabosch"
+    highway "motorway_link"
+    length 17583.0
+    lanes 2
+  ]
+  edge [
+    source 8
+    target 10
+    name "Knooppunt Zonzeel->Knooppunt Princeville"
+    highway "motorway_link"
+    length 6314.0
+    lanes 4
+  ]
+  edge [
+    source 8
+    target 14
+    name "Knooppunt Ridderkerk->Knooppunt Zonzeel"
+    highway "motorway_link"
+    length 18045.0
+    lanes 4
+  ]
+  edge [
+    source 10
+    target 12
+    name "Knooppunt Sint-Annabosch->Knooppunt Princeville"
+    highway "motorway_link"
+    length 11822.0
+    lanes 4
+  ]
+  edge [
+    source 11
+    target 14
+    name "Knooppunt Terbregseplein->Knooppunt Ridderkerk"
+    highway "motorway_link"
+    length 2426.0
+    lanes 3
+  ]
+  edge [
+    source 11
+    target 13
+    name "Knooppunt Terbregseplein->Knooppunt Gouwe"
+    highway "motorway_link"
+    length 11557.0
+    lanes 2
+  ]
+  edge [
+    source 13
+    target 15
+    name "Knooppunt Oudenrijn->Knooppunt Gouwe"
+    highway "motorway_link"
+    length 23340.0
+    lanes 5
+  ]
+]
